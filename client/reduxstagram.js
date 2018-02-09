74e680eb004a89b131fe7b0c363b8905311e7e7b
Luis Miguel Del Gallego <alucardluis@gmail.com>
2018-02-09 17:27:44 -0500

    First commit

---
+++ client/reduxstagram.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render } from 'react-dom';
+
+//import css
+import css from './styles/style.styl';
+
+//import
+import Main from './components/Main';
+
+render(<Main/>, document.getElementById('root'));
